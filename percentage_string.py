ae9c0087782ee6c0468b6d635a883d22076ec889
Yonish18 <151393231+Yonish18@users.noreply.github.com>
2023-11-18 21:03:17 -0500

    Add files via upload

---
+++ percentage_string.py
@@ -0,0 +1,35 @@
+def interfaceworkerdict(days_colors):
+    count_colors = {}
+    color_percentages = {}
+    percentage_string = ""
+    for day in days_colors:
+        if days_colors[day] not in count_colors:
+            count_colors[days_colors[day]] = 1 
+        else:
+            count_colors[days_colors[day]] += 1
+    for color in count_colors:
+        color_percentages[color] = count_colors[color]/len(days_colors) * 100
+                                                
+    for color in color_percentages:
+        percentage_string += (color + ": " + str(color_percentages[color]) + '% ,')
+    print(percentage_string)
+    
+def interfaceworkerlist(days_colors):
+    count_colors = {}
+    color_percentages = {}
+    percentage_string = ""
+    for color in days_colors:
+        if color not in count_colors:
+            count_colors[color] = 1 
+        else:
+            count_colors[color] += 1
+    for color in count_colors:
+        color_percentages[color] = count_colors[color]/len(days_colors) * 100
+                                                
+    for color in color_percentages:
+        percentage_string += (color + ": " + str(color_percentages[color]) + '% ,')
+    print(percentage_string)
+    
+    
+
+    
