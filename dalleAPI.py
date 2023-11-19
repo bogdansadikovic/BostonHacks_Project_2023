a054283e268e9338fdc0efd6ef8f84c5474052af
casey <111756500+cj-casey@users.noreply.github.com>
2023-11-18 23:18:04 -0500

    Add files via upload

---
+++ dalleAPI.py
@@ -0,0 +1,41 @@
+import requests
+import streamlit
+
+
+# Set your OpenAI API key
+def apiCall(prompt):
+    api_key = "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"
+
+    # Define the API endpoint URL
+    url = "https://api.openai.com/v1/images/generations"
+
+    # Define the request headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+
+    }
+
+    # Define the request payload as a Python dictionary
+    payload = {
+        "model": "dall-e-3",
+        "prompt": prompt,
+        "n": 1,
+        "size": "1024x1024"
+    }
+
+    # Make the POST request to the API using the requests library
+    response = requests.post(url, json=payload, headers=headers)
+
+    # Check the response status code (200 indicates success)
+    if response.status_code == 200:
+        # Parse the response JSON
+        data = response.json()
+        # Get the URL of the generated image from the response
+        image_url = data['data'][0]['url']
+        # Print the image URL
+        return image_url
+    else:
+        streamlit.write(response.status_code)
+        streamlit.write("The image could not be generated")
+        return 0
---
+++ stringGenerator.py
@@ -0,0 +1,42 @@
+import math
+
+def interfaceworkerdict(days_colors):
+    count_colors = {}
+    color_percentages = {}
+    percentage_string = ""
+    for day in days_colors:
+        if days_colors[day] not in count_colors:
+            count_colors[days_colors[day]] = 1
+        else:
+            count_colors[days_colors[day]] += 1
+    for color in count_colors:
+        color_percentages[color] = int(count_colors[color] / len(days_colors) * 100)
+
+    for color in color_percentages:
+        percentage_string += (color + ": " + str(color_percentages[color]) + '% ,')
+
+    percentage_string = percentage_string.replace('RED','#F7251C')
+    percentage_string = percentage_string.replace('GREEN', '#227F19')
+    percentage_string = percentage_string.replace('BLUE', ' #000EF9')
+    percentage_string = percentage_string.replace('VIOLET', '#E886EB')
+    percentage_string = percentage_string.replace('ORANGE', '#FBA72F')
+    percentage_string = percentage_string.replace('GREY', '#808080')
+
+    return percentage_string
+
+
+def interfaceworkerlist(days_colors):
+    count_colors = {}
+    color_percentages = {}
+    percentage_string = ""
+    for color in days_colors:
+        if color not in count_colors:
+            count_colors[color] = 1
+        else:
+            count_colors[color] += 1
+    for color in count_colors:
+        color_percentages[color] = count_colors[color] / len(days_colors) * 100
+
+    for color in color_percentages:
+        percentage_string += (color + ": " + str(color_percentages[color]) + '% ,')
+    return percentage_string
